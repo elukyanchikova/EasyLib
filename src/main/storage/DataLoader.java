68a65fc09d839baf3de5a02eff1871414e9fabd4
SkarletSkay <meskarlet123@gmail.com>
2018-02-28 18:17:05 +0300

    Add template for data loading

--- src/main/storage/DataLoader.java
+++ src/main/storage/DataLoader.java
@@ -1,15 +1,26 @@
 package storage;
 
+import org.json.JSONObject;
+
+import java.io.*;
+
 public class DataLoader {
 
-    //private JSONObject jsonData;
+    private JSONObject jsonData;
 
     public void start(){
 
     }
 
-    public void end(){
-
+    public void save(){
+        try {
+            JSONObject root = new JSONObject();
+            PrintWriter pw = new PrintWriter(new FileOutputStream(
+                    new File("library.json")));
+            pw.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update(){
@@ -17,11 +28,23 @@ public class DataLoader {
     }
 
     private void load(){
-
+        File file = new File("library.json");
+        if(file.exists()) {
+            try {
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(
+                                new FileInputStream(file)));
+                StringBuilder stringBuilder = new StringBuilder();
+                reader.lines().forEach(stringBuilder::append);
+                this.jsonData = new JSONObject(stringBuilder.toString());
+                reader.close();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void get(String key){
-        
+    public JSONObject get(String key){
+        return null;
     }
-
 }
