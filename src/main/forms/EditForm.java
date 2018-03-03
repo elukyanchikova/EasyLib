beed6799e04c7b38971bdff59ca874798c72d25c
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2018-03-03 19:02:25 +0300

    DO add EditForm class and fxml

---
+++ src/main/forms/EditForm.java
@@ -0,0 +1,68 @@
+package forms;
+
+import documents.Storage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import users.Guest;
+import users.Session;
+import users.Student;
+
+public class EditForm {
+
+    private Stage stage;
+    private Scene scene;
+
+    @FXML private Button addFileBtn;
+    @FXML private Button deleteFileBtn;
+    @FXML private Button modifyFileBtn;
+
+    @FXML private Button addUserBtn;
+    @FXML private Button deleteUserBtn;
+    @FXML private Button modifyUserBtn;
+
+    /**
+     * Initialization and run new scene on the primary stage
+     */
+    void startForm(Stage primaryStage) throws Exception{
+        this.stage = primaryStage;
+        sceneInitialization();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Initialization scene and scene's elements
+     */
+    private void sceneInitialization() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("EditForm.fxml"));
+        loader.setController(this);
+        GridPane root = loader.load();
+        this.scene = new Scene(root,700,700);
+
+        deleteFileBtn = (Button) scene.lookup("#deleteFileBtn");
+        addFileBtn = (Button) scene.lookup("#addFileBtn");
+        modifyFileBtn = (Button) scene.lookup("#modifyFileBtn");
+
+        deleteUserBtn = (Button) scene.lookup("#deleteUserBtn");
+        addUserBtn = (Button) scene.lookup("#addUserBtn");
+        modifyUserBtn = (Button) scene.lookup("#modifyUSerBtn");
+
+    }
+
+    /**
+     * Click on button "addUser" event
+     * If the User has an access to editing(i.e. librian) than the inf can be edited by him
+     */
+    @FXML
+    public void addUser() throws Exception{
+
+
+        //if()
+        {
+        }
+    }
+}
