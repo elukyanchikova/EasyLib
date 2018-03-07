cb2e5ba487582d9b5bd0865667209c253cf0477b
SkarletSkay <meskarlet123@gmail.com>
2018-03-07 16:03:26 +0300

    Add edition

--- src/main/documents/Book.java
+++ src/main/documents/Book.java
@@ -8,29 +8,31 @@ import java.util.ArrayList;
 public class Book extends Document {
     public String publisher;
     public int year;
+    public String edition;
     public boolean isBestseller;
 
     public Book(int id, String title, ArrayList<String> authors, ArrayList<String> keywords, int price,
                  int numberOfRequests, ArrayList<Copy> availableCopies, ArrayList<Copy> takenCopies, int lastCopyID,
-                 String publisher, int year, boolean isBestseller) {
+                 String publisher, int year,String edition, boolean isBestseller) {
 
         super(id, title, "Book", authors, keywords, price, numberOfRequests, availableCopies, takenCopies, lastCopyID);
         this.publisher = publisher;
         this.year = year;
+        this.edition = edition;
         this.isBestseller = isBestseller;
         this.checkOutTime = 21;
     }
 
     public Book(String title, ArrayList<String> authors, ArrayList<String> keywords, int price,
-                ArrayList<Copy> copies, String publisher, int year, boolean isBestseller) {
+                ArrayList<Copy> copies, String publisher, int year,String edition, boolean isBestseller) {
 
-        this(++lastID, title, authors, keywords, price,0, copies, new ArrayList<>(), 1, publisher, year, isBestseller);
+        this(++lastID, title, authors, keywords, price,0, copies, new ArrayList<>(), 1, publisher, year,edition, isBestseller);
     }
 
     public Book(String title, ArrayList<String> authors, ArrayList<String> keywords, int price,
-                 String publisher, int year, boolean isBestseller) {
+                 String publisher, int year, String edition, boolean isBestseller) {
 
-        this(++lastID, title, authors, keywords, price,0, new ArrayList<>(), new ArrayList<>(), 1, publisher, year, isBestseller);
+        this(++lastID, title, authors, keywords, price,0, new ArrayList<>(), new ArrayList<>(), 1, publisher, year, edition,isBestseller);
     }
 
     public Book(int id, JSONObject data, Database database){
@@ -38,6 +40,7 @@ public class Book extends Document {
         this.docType = "Book";
         this.isBestseller = data.getBoolean("Bestseller");
         this.publisher = data.getString("Publisher");
+        this.edition = data.getString("Edition");
         this.year = data.getInt("Year");
     }
 
@@ -54,6 +57,7 @@ public class Book extends Document {
         JSONObject data = super.serialize();
         data.put("Publisher", publisher);
         data.put("Year", year);
+        data.put("Edition", edition);
         data.put("Bestseller", isBestseller);
         return data;
     }
