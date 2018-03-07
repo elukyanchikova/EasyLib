c536dcc4b667476b17132e9534107b5007c7c92b
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2018-03-07 10:40:17 +0300

    DO add templates for Tests

---
+++ src/test/TestCases2.java
@@ -0,0 +1,95 @@
+import documents.Book;
+import documents.Copy;
+import documents.Document;
+import forms.MainForm;
+import org.junit.Test;
+import users.*;
+
+import java.util.ArrayList;
+
+
+public class TestCases2 {
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase1() {
+    }
+
+
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase2() {
+    }
+
+
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase3() {
+    }
+
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase4() {
+    }
+
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase5() {
+    }
+
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase6() {
+    }
+
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase7() {
+    }
+
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase8() {
+    }
+
+    /**
+     * Initial state:
+     * Action:
+     * Effect:
+     */
+    @Test
+    public void TestCase9() {
+    }
+
+}
