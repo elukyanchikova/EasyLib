08b40bcdc7addc1def4b9dab1390f1f6fc30f1c7
SkarletSkay <meskarlet123@gmail.com>
2018-03-31 00:36:22 +0300

    add form call

--- src/main/java/documents/Document.java
+++ src/main/java/documents/Document.java
@@ -141,8 +141,8 @@ public abstract class Document {
         return docType;
     }
 
-    public void putInPQ(UserCard user, Document document, DatabaseManager databaseManager){
-       document.requestedBy.add(user);
+    public void putInPQ(UserCard user, DatabaseManager databaseManager){
+       this.requestedBy.add(user);
     }
 
     public int getNumberOfRequests() {
