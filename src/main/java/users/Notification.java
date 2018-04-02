989b3b0a159b44cfd6ac833a01f75a04cc230dc0
SkarletSkay <meskarlet123@gmail.com>
2018-04-02 09:22:07 +0300

    add notification ids

---
+++ src/main/java/users/Notification.java
@@ -0,0 +1,11 @@
+package users;
+
+public class Notification {
+
+    public static final int NO_NOTIFICATION = 0;
+    public static final int GET_COPY_NOTIFICATION = 1;
+    public static final int OUTDATNDING_REQUEST_NOTIFICATION = 2;
+    public static final int ACCEPT_NOTIFICATION = 3;
+    public static final int REQECT_NOTIFICATION = 4;
+
+}
