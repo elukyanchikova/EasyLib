47527d7d2a387de7a7285864dad8fcedf8404e21
Mukhutdinov Jameel <gamingjamtv@gmail.com>
2018-04-04 14:43:00 +0300

    add testcases 4 5 6

--- src/test/java/TestCases3.java
+++ src/test/java/TestCases3.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import storage.DatabaseManager;
 import users.*;
 
+import javax.jws.soap.SOAPBinding;
 import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -183,14 +184,364 @@ public class TestCases3 {
 
     @Test
     public void Test4() {
+
+        ArrayList<Copy> l1_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> l1_requestedDocuments = new ArrayList<Document>();
+
+        UserCard librarian_1 = new UserCard("Irma", "Pins", new Librarian(), "8981351785", "north of London",
+                l1_checkedOutCopies, l1_requestedDocuments);
+        databaseManager.saveUserCard(librarian_1);
+        Session session = new Session(databaseManager.getUserCard(librarian_1.getId()).userType, 29, 3);
+        session.userCard = librarian_1;
+
+        ArrayList<Copy> p1_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> p1_requestedDocs = new ArrayList<Document>();
+        UserCard p1 = new UserCard(1010, "Sergey", "Afonso", new Faculty(), "30001", "Via Margutta, 3", p1_checkedOutCopies, p1_requestedDocs);
+        databaseManager.saveUserCard(p1);
+
+        /////////////////////////////////////////////////////////////////
+
+        ArrayList<Copy> s_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> s_requestedDocs = new ArrayList<Document>();
+        UserCard s = new UserCard(1101, "Andrey", "Velo", new Student(), "30004", "Avenida Mazatlan 250", s_checkedOutCopies, s_requestedDocs);
+        databaseManager.saveUserCard(s);
+
+        //////////////////////////////////////////////////////////////////
+
+        ArrayList<Copy> v_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> v_requestedDocs = new ArrayList<Document>();
+        UserCard v = new UserCard(1110, "Veronika", "Rama", new VisitingProfessor(), "30005", "Stret Atocha, 27", v_checkedOutCopies, v_requestedDocs);
+        databaseManager.saveUserCard(v);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ArrayList<Copy> b1_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b1_authors = new ArrayList<String>();
+        b1_authors.add("Thomas H. Cormen");
+        b1_authors.add("Charles E. Leiserson");
+        b1_authors.add("Ronald L. Rivest");
+        b1_authors.add("Clifford Stein");
+
+        ArrayList<String> b1_keywords = new ArrayList<String>();
+        b1_keywords.add("none");
+
+        Book b1 = new Book("Introduction to Algorithms", b1_authors,
+                b1_keywords, 5000, b1_copies, "MIT Press", 2009, "Third edition", false);
+        databaseManager.saveDocuments(b1);
+
+        b1.setCopy(new Copy(b1, 1, 1));
+        b1.setCopy(new Copy(b1, 2, 1));
+        b1.setCopy(new Copy(b1, 3, 1));
+
+        databaseManager.saveDocuments(b1);
+
+        /////////////////////////////////////////////////////////////
+        ArrayList<Copy> b2_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b2_authors = new ArrayList<String>();
+        b2_authors.add("Erich Gamma");
+        b2_authors.add("Ralph Johnson");
+        b2_authors.add("John Vlissides");
+        b2_authors.add("Richard Helm");
+
+        ArrayList<String> b2_keywords = new ArrayList<String>();
+        b2_keywords.add("none");
+
+        Book b2 = new Book("Design Patterns: Elements of Reusable Object-Oriented Software", b2_authors,
+                b2_keywords, 1700, b2_copies, "Addison-Wesley Professional", 2003, "First edition", true);
+        databaseManager.saveDocuments(b2);
+        b2.setCopy(new Copy(b2, 1, 1));
+        b2.setCopy(new Copy(b2, 2, 1));
+        b2.setCopy(new Copy(b2, 3, 1));
+
+        databaseManager.saveDocuments(b2);
+
+        ///////////////////////////////////////////////////////////////
+        ArrayList<Copy> b3_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b3_authors = new ArrayList<String>();
+        b3_authors.add("Tony Hoare");
+
+        ArrayList<String> b3_keywords = new ArrayList<String>();
+        b3_keywords.add("none");
+
+        Book b3 = new Book("Null References: The Billion Dollar Mistake", b3_authors,
+                b3_keywords, 700, b3_copies, "none", 2018, "none", false);
+        b3.setReference(true);
+        b3.setCopy(new Copy(b3, 1, 1));
+        b3.setCopy(new Copy(b3, 1, 2));
+
+        databaseManager.saveDocuments(b3);
+
+        /////////////////////////////////////////////////////////////////////
+
+        b1.takeCopy(p1, session);
+        databaseManager.saveDocuments(b1);
+        databaseManager.saveUserCard(p1);
+        b2.takeCopy(s, session);
+        databaseManager.saveDocuments(b2);
+        databaseManager.saveUserCard(s);
+        b2.takeCopy(v, session);
+        databaseManager.saveDocuments(b2);
+        databaseManager.saveUserCard(v);
+
+        ///////////////////////////////////////////////////////////////////////////
+        //initialState();
+        ///////////////////////////////////////////////////////////////////////////
+
+        b2.deletePQ();
+        for (int i = 0; i <b2.bookedCopies.size() ; i++) {
+            b2.availableCopies.add(b2.bookedCopies.get(i));
+        };
+        databaseManager.saveDocuments(b2);
+
+
+
+
+
     }
 
     @Test
     public void Test5() {
+
+        ArrayList<Copy> l1_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> l1_requestedDocuments = new ArrayList<Document>();
+
+        UserCard librarian_1 = new UserCard("Irma", "Pins", new Librarian(), "8981351785", "north of London",
+                l1_checkedOutCopies, l1_requestedDocuments);
+        databaseManager.saveUserCard(librarian_1);
+        Session session = new Session(databaseManager.getUserCard(librarian_1.getId()).userType, 29, 3);
+        session.userCard = librarian_1;
+
+        ArrayList<Copy> p1_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> p1_requestedDocs = new ArrayList<Document>();
+        UserCard p1 = new UserCard(1010, "Sergey", "Afonso", new Faculty(), "30001", "Via Margutta, 3", p1_checkedOutCopies, p1_requestedDocs);
+        databaseManager.saveUserCard(p1);
+
+        /////////////////////////////////////////////////////////////////
+
+        ArrayList<Copy> s_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> s_requestedDocs = new ArrayList<Document>();
+        UserCard s = new UserCard(1101, "Andrey", "Velo", new Student(), "30004", "Avenida Mazatlan 250", s_checkedOutCopies, s_requestedDocs);
+        databaseManager.saveUserCard(s);
+
+        //////////////////////////////////////////////////////////////////
+
+        ArrayList<Copy> v_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> v_requestedDocs = new ArrayList<Document>();
+        UserCard v = new UserCard(1110, "Veronika", "Rama", new VisitingProfessor(), "30005", "Stret Atocha, 27", v_checkedOutCopies, v_requestedDocs);
+        databaseManager.saveUserCard(v);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ArrayList<Copy> b1_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b1_authors = new ArrayList<String>();
+        b1_authors.add("Thomas H. Cormen");
+        b1_authors.add("Charles E. Leiserson");
+        b1_authors.add("Ronald L. Rivest");
+        b1_authors.add("Clifford Stein");
+
+        ArrayList<String> b1_keywords = new ArrayList<String>();
+        b1_keywords.add("none");
+
+        Book b1 = new Book("Introduction to Algorithms", b1_authors,
+                b1_keywords, 5000, b1_copies, "MIT Press", 2009, "Third edition", false);
+        databaseManager.saveDocuments(b1);
+
+        b1.setCopy(new Copy(b1, 1, 1));
+        b1.setCopy(new Copy(b1, 2, 1));
+        b1.setCopy(new Copy(b1, 3, 1));
+
+        databaseManager.saveDocuments(b1);
+
+        /////////////////////////////////////////////////////////////
+        ArrayList<Copy> b2_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b2_authors = new ArrayList<String>();
+        b2_authors.add("Erich Gamma");
+        b2_authors.add("Ralph Johnson");
+        b2_authors.add("John Vlissides");
+        b2_authors.add("Richard Helm");
+
+        ArrayList<String> b2_keywords = new ArrayList<String>();
+        b2_keywords.add("none");
+
+        Book b2 = new Book("Design Patterns: Elements of Reusable Object-Oriented Software", b2_authors,
+                b2_keywords, 1700, b2_copies, "Addison-Wesley Professional", 2003, "First edition", true);
+        databaseManager.saveDocuments(b2);
+        b2.setCopy(new Copy(b2, 1, 1));
+        b2.setCopy(new Copy(b2, 2, 1));
+        b2.setCopy(new Copy(b2, 3, 1));
+
+        databaseManager.saveDocuments(b2);
+
+        ///////////////////////////////////////////////////////////////
+        ArrayList<Copy> b3_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b3_authors = new ArrayList<String>();
+        b3_authors.add("Tony Hoare");
+
+        ArrayList<String> b3_keywords = new ArrayList<String>();
+        b3_keywords.add("none");
+
+        Book b3 = new Book("Null References: The Billion Dollar Mistake", b3_authors,
+                b3_keywords, 700, b3_copies, "none", 2018, "none", false);
+        b3.setReference(true);
+        b3.setCopy(new Copy(b3, 1, 1));
+        b3.setCopy(new Copy(b3, 1, 2));
+
+        databaseManager.saveDocuments(b3);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //initialState
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        b3.takeCopy(p1, session);
+        databaseManager.saveDocuments(b3);
+        databaseManager.saveUserCard(p1);
+        b3.takeCopy(s, session);
+        databaseManager.saveDocuments(b3);
+        databaseManager.saveUserCard(s);
+        MainForm mainForm = new MainForm();
+
+        session = new Session(v.userType, 29, 3);
+        session.userCard = v;
+        mainForm.setSession(session);
+        mainForm.request(b3);
+
+        session = new Session(databaseManager.getUserCard(librarian_1.getId()).userType, 29, 3);
+        session.userCard = librarian_1;
+
+
+        Assert.assertEquals(v,b3.requestedBy.peek());
+
+
     }
 
     @Test
     public void Test6() {
+
+        ArrayList<Copy> l1_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> l1_requestedDocuments = new ArrayList<Document>();
+
+        UserCard librarian_1 = new UserCard("Irma", "Pins", new Librarian(), "8981351785", "north of London",
+                l1_checkedOutCopies, l1_requestedDocuments);
+        databaseManager.saveUserCard(librarian_1);
+        Session session = new Session(databaseManager.getUserCard(librarian_1.getId()).userType, 29, 3);
+        session.userCard = librarian_1;
+
+        ArrayList<Copy> p1_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> p1_requestedDocs = new ArrayList<Document>();
+        UserCard p1 = new UserCard(1010, "Sergey", "Afonso", new Faculty(), "30001", "Via Margutta, 3", p1_checkedOutCopies, p1_requestedDocs);
+        databaseManager.saveUserCard(p1);
+
+        /////////////////////////////////////////////////////////////////
+
+        ArrayList<Copy> p2_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> p2_requestedDocs = new ArrayList<Document>();
+        UserCard p2 = new UserCard(1011, "Nadia", "Teixeira", new Student(), "30002", "Via Sacra, 13", p2_checkedOutCopies, p2_requestedDocs);
+        databaseManager.saveUserCard(p2);
+
+        //////////////////////////////////////////////////////////////////
+
+        ArrayList<Copy> p3_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> p3_requestedDocs = new ArrayList<Document>();
+        UserCard p3 = new UserCard(1100, "Elvira", "Espindola", new Student(), "30003", "Via del Corso, 22", p3_checkedOutCopies, p3_requestedDocs);
+        databaseManager.saveUserCard(p3);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        ArrayList<Copy> s_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> s_requestedDocs = new ArrayList<Document>();
+        UserCard s = new UserCard(1101, "Andrey", "Velo", new Student(), "30004", "Avenida Mazatlan 250", s_checkedOutCopies, s_requestedDocs);
+        databaseManager.saveUserCard(s);
+
+        //////////////////////////////////////////////////////////////////
+
+        ArrayList<Copy> v_checkedOutCopies = new ArrayList<Copy>();
+        ArrayList<Document> v_requestedDocs = new ArrayList<Document>();
+        UserCard v = new UserCard(1110, "Veronika", "Rama", new VisitingProfessor(), "30005", "Stret Atocha, 27", v_checkedOutCopies, v_requestedDocs);
+        databaseManager.saveUserCard(v);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ArrayList<Copy> b1_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b1_authors = new ArrayList<String>();
+        b1_authors.add("Thomas H. Cormen");
+        b1_authors.add("Charles E. Leiserson");
+        b1_authors.add("Ronald L. Rivest");
+        b1_authors.add("Clifford Stein");
+
+        ArrayList<String> b1_keywords = new ArrayList<String>();
+        b1_keywords.add("none");
+
+        Book b1 = new Book("Introduction to Algorithms", b1_authors,
+                b1_keywords, 5000, b1_copies, "MIT Press", 2009, "Third edition", false);
+        databaseManager.saveDocuments(b1);
+
+        b1.setCopy(new Copy(b1, 1, 1));
+        b1.setCopy(new Copy(b1, 2, 1));
+        b1.setCopy(new Copy(b1, 3, 1));
+
+        databaseManager.saveDocuments(b1);
+
+        /////////////////////////////////////////////////////////////
+        ArrayList<Copy> b2_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b2_authors = new ArrayList<String>();
+        b2_authors.add("Erich Gamma");
+        b2_authors.add("Ralph Johnson");
+        b2_authors.add("John Vlissides");
+        b2_authors.add("Richard Helm");
+
+        ArrayList<String> b2_keywords = new ArrayList<String>();
+        b2_keywords.add("none");
+
+        Book b2 = new Book("Design Patterns: Elements of Reusable Object-Oriented Software", b2_authors,
+                b2_keywords, 1700, b2_copies, "Addison-Wesley Professional", 2003, "First edition", true);
+        databaseManager.saveDocuments(b2);
+        b2.setCopy(new Copy(b2, 1, 1));
+        b2.setCopy(new Copy(b2, 2, 1));
+        b2.setCopy(new Copy(b2, 3, 1));
+
+        databaseManager.saveDocuments(b2);
+
+        ///////////////////////////////////////////////////////////////
+        ArrayList<Copy> b3_copies = new ArrayList<Copy>();
+
+        ArrayList<String> b3_authors = new ArrayList<String>();
+        b3_authors.add("Tony Hoare");
+
+        ArrayList<String> b3_keywords = new ArrayList<String>();
+        b3_keywords.add("none");
+
+        Book b3 = new Book("Null References: The Billion Dollar Mistake", b3_authors,
+                b3_keywords, 700, b3_copies, "none", 2018, "none", false);
+        b3.setReference(true);
+        b3.setCopy(new Copy(b3, 1, 1));
+        b3.setCopy(new Copy(b3, 1, 2));
+
+        databaseManager.saveDocuments(b3);
+
+        b3.takeCopy(p1, session);
+        databaseManager.saveDocuments(b3);
+        databaseManager.saveUserCard(p1);
+        b3.takeCopy(s, session);
+        databaseManager.saveDocuments(b3);
+        databaseManager.saveUserCard(s);
+        b3.takeCopy(v, session);
+        databaseManager.saveDocuments(b3);
+        databaseManager.saveUserCard(v);
+        b3.takeCopy(p2, session);
+        databaseManager.saveDocuments(b3);
+        databaseManager.saveUserCard(p2);
+        b3.takeCopy(p3, session);
+        databaseManager.saveDocuments(b3);
+        databaseManager.saveUserCard(p3);
+
+        UserCard[] pq = new UserCard[b3.requestedBy.size()];
+
     }
 
     @Test
