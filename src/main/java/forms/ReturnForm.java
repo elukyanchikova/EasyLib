83d852d5548e4e360ee415906004a7ed1deffdf6
Mukhutdinov Jameel <gamingjamtv@gmail.com>
2018-04-04 16:03:15 +0300

    Improve

--- src/main/java/forms/ReturnForm.java
+++ src/main/java/forms/ReturnForm.java
@@ -340,7 +340,6 @@ public class ReturnForm {
             databaseManager.saveUserCard(users[i]);
         }
         doc.deletePQ();
-        databaseManager.saveDocuments(doc);
     }
 
     private void autobooking(Document document){
