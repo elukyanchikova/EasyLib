a199bab27ab8771cc2fabe629833319d975391f0
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2018-04-23 17:28:06 +0300

    add TestCases class

---
+++ src/test/java/TestCases4.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class TestCases4 {
+    @Test
+    public void Test1(){}
+    @Test
+    public void Test2(){}
+    @Test
+    public void Test3(){}
+    @Test
+    public void Test4(){}
+    @Test
+    public void Test5(){}
+    @Test
+    public void Test6(){}
+    @Test
+    public void Test7(){}
+    @Test
+    public void Test8(){}
+
+}
